0146352b56bf65b80c081099e7b7c43c9da193c2
halle <halle.mills.2022@student.lhschools.org>
2019-12-16 09:55:46 -0500

    Random Sentence Completed

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -12,8 +12,6 @@ public class Main {
         int[] array = createNumberArray(size, lowestNumber, highestNumber);
 
 
-
-
 //        Pattern1();
 //        Pattern2();
 //        Pattern3();
@@ -29,10 +27,22 @@ public class Main {
 
         String[] adjectives = {"Bashful", "Shiny", "Fast", "Plain"};
         String[] places = {"Hawaii", "Floria", "Arizona", "Japan"};
-        String[] people = {"Chris Brown", "Billie", "Harry Styles", "Nemo" };
-        String[] verbs = {"Dry", "Paint", "Close", "Love"};
+        String[] people = {"Chris Brown", "Billie", "Harry Styles", "Nemo"};
+        String[] verb = {"Dry", "Paint", "Close", "Love"};
         String[] times = {"1:00", "11:03", "9:36", "6:45"};
-        String[] things = {"Food", "Shoes", "Computer", "Bed"}; 
+        String[] things = {"Food", "Shoes", "Computer", "Bed"};
+
+
+        System.out.println("randomword" + people);
+        System.out.print("I was" + " " + sentencegenerator(verb) + " " + " " + "down the" + " " + sentencegenerator(places) + " ");
+        System.out.println("Around" + " " + sentencegenerator(times) + " " + "with my bestfriend" + " " + sentencegenerator(people) + " ");
+        System.out.print("And we" + " " + sentencegenerator(verb) + " " + " " + "a" + " " + sentencegenerator(adjectives) + " " + " " + sentencegenerator(things) + " ");
+        System.out.print("That was" + " " + sentencegenerator(verb) + " " + sentencegenerator(things) + " ");
+        String[] part1 = {"happy", "biggin", "short",};
+        String[] part2 = {"sand", "comfy", "sticky",};
+        String[] part3 = {"dory", "water", "period",};
+        System.out.println(print80srappername(part1) + " " + print80srappername(part2) + " " + print80srappername(part3));
+
 
     }
 
@@ -61,45 +71,13 @@ public class Main {
         return number;
     }
 
-    public static int sum(int[] array) {
-        int sum = 0;
-
-        for (int i = 0; i < array.length; i++) {
-            sum += array[i];
-
-        }
 
 
-        return sum;
-
-    }
-
-    public static int minimum(int[] array) {
-        int minimum = array[0];
-        for (int i = 1; i < array.length; i++) {
-            if (minimum > array[i]) {
-                minimum= array[i];
-            }
-        }
-        return minimum;
-    }
-
-    public static int maximum(int[] array) {
-        int maximum = array[0];
-        for(int i = 1; i < array.length; i++) {
-            if (maximum < array[i]) {
-                maximum= array[i];
-            }
-        }
-
-        return maximum;
-    }
-
-    public static void Pattern1() {
+        public static void Pattern1() {
         for (int i = 0; i < 6; i++) {
 
             for (int columns = 0; columns < 6; columns++) {
-                
+
                 System.out.print("*");
             }
             System.out.println();
@@ -148,7 +126,7 @@ public class Main {
     public static void Pattern5() {
         for (int rows = 1; rows <= 6; rows++) {
 
-            for (int columns = 1; columns <=rows; columns++) {
+            for (int columns = 1; columns <= rows; columns++) {
                 System.out.print(rows);
             }
             System.out.println();
@@ -157,7 +135,71 @@ public class Main {
 
 
 
+
+    public static int sum(int[] array) {
+        int sum = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+
+        }
+
+
+        return sum;
+
     }
+
+    public static int minimum(int[] array) {
+        int minimum = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (minimum > array[i]) {
+                minimum = array[i];
+            }
+        }
+        return minimum;
+    }
+
+    public static int maximum(int[] array) {
+        int maximum = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (maximum < array[i]) {
+                maximum = array[i];
+            }
+        }
+
+        return maximum;
+
+
+    }
+
+    public static String sentencegenerator(String[] array) {
+        Random random = new Random();
+        return array[random.nextInt(array.length)];
+
+
+    }
+
+    public static String randomword(String[] array) {
+        Random random = new Random();
+        return array[random.nextInt(array.length)];
+
+
+    }
+
+    public static String print80srappername(String[] array) {
+        Random random = new Random();
+        return array[random.nextInt(array.length)];
+
+
+    }
+
+
+
+
+
+
+
+
 
 
 
